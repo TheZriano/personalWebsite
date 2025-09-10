995e1010316815ea2a8c75e4185844719453031c
Giacomo Montenegro <giacomo.montenegro09@gmail.com>
2025-09-11 00:48:56 +0200

    aggiunta frontend e nackend (no databse) di /projects

--- main/server.js
+++ main/server.js
@@ -15,24 +15,29 @@ app.use(cookieParser());
 app.use(cors({origin:"http://localhost:3000", credentials: true}));
 app.use(express.static(path.join(__dirname, "frontend")));
 
-
+//*ROUTES FRONTEND
 app.get("/", (req,res) => {
-    
     res.sendFile(path.join(__dirname,"frontend/homepage.html"))
-
 });
+
 app.get("/contacts", (req,res) => {
-    
     res.sendFile(path.join(__dirname,"frontend/contacts.html"))
-
 });
+
 app.get("/projects", (req,res) => {
-    
     res.sendFile(path.join(__dirname,"frontend/portfolio.html"))
-
 });
 
-
+//*ROUTES API
+app.get("/api/projects", async (req,res) => {
+    try{
+        const projects= await database.getProjects();
+        res.json(projects);
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send("Server error");
+    }
+});
 
 
 app.listen(port);
